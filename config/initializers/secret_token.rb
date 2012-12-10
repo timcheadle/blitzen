0ac5fc0c166702c576ebeb50cabcb15ccceb68bf
Tim Cheadle <tim@rationalmeans.com>
2012-12-09 20:45:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blitzen::Application.config.secret_token = '6fc4fab26516638a55cadb902d3f03d4349b2b5c267baa09a050d056c8dd416f13fa947c7e838e37fb22ec885f00d135143b1baa591298d952a8292ae482368f'
